6cd4b44fb10d0c5af048adee227b2a4153bc9db6
LocUEH <locdinh.31221020226@st.ueh.edu.vn>
2024-04-22 01:43:48 +0700

    postgreSQL database settings

--- ueh-web-dev/backend/accounts/views.py
+++ ueh-web-dev/backend/accounts/views.py
@@ -1,11 +1,6 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.shortcuts import render
 
-app_name = 'accounts'
+# Create your views here.
 
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('signup/', views.SignUp.as_view(), name="signup"),
-]
+def index(request):
+    return render(request, 'index.html')
--- ueh-web-dev/backend/backend/settings.py
+++ ueh-web-dev/backend/backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+#load env variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,8 +93,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_mame'),
+        'USER': os.getenv('DB_user'),
+        'PASSWORD': os.getenv('DB_password'),
+        'HOST': 'localhost',
+        'PORT': os.getenv('DB_port'),
     }
 }
 
