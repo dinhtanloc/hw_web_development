3838ffbbedd70d9dd0191b64c0c058cf809c5a78
LocUEH <locdinh.31221020226@st.ueh.edu.vn>
2024-05-03 23:37:31 +0700

    edit update backend

--- ueh-web-dev/backend/accounts/urls.py
+++ ueh-web-dev/backend/accounts/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('test/', views.testEndPoint, name='test'),
-    path('profile/', views.get_profile, name='profile'),
+    path('profile/', views.profileView, name='profile'),
     path('', views.getRoutes),
 ]
--- ueh-web-dev/backend/accounts/views.py
+++ ueh-web-dev/backend/accounts/views.py
@@ -52,6 +52,7 @@ def getRoutes(request):
 def testEndPoint(request):
     if request.method == 'GET':
         user_info = {
+            'id': request.user.id,
             'username': request.user.username,
             'email': request.user.email,
             # Thêm các thông tin khác của người dùng mà bạn muốn hiển thị
@@ -65,12 +66,43 @@ def testEndPoint(request):
     return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_profile(request):
-    try:
-        profile = Profile.objects.get(user=request.user)
-        serializer = ProfileSerializer(profile)
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def get_profile(request):
+#     try:
+#         profile = Profile.objects.get(user=request.user)
+#         serializer = ProfileSerializer(profile)
+#         return Response(serializer.data)
+#     except Profile.DoesNotExist:
+#         return Response({"error": "Profile does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    
+# class UserProfileView(APIView):
+#     def get(self, request):
+#         user_profile = Profile.objects.get(user=request.user)
+#         serializer = ProfileSerializer(user_profile)
+#         return Response(serializer.data)
+
+#     def put(self, request):
+#         user_profile = Profile.objects.get(user=request.user)
+#         serializer = ProfileSerializer(user_profile, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])    
+def profileView(request):
+    if request.method =="GET":
+        user_profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(user_profile)
         return Response(serializer.data)
-    except Profile.DoesNotExist:
-        return Response({"error": "Profile does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    elif request.method =="PUT":
+        user_profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
