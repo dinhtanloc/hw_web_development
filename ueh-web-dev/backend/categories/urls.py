759537c99f9a1d98cbc43eec544eb6c4fb5d7232
LocUEH <locdinh.31221020226@st.ueh.edu.vn>
2024-05-21 20:56:50 +0700

    update list view

--- ueh-web-dev/backend/categories/urls.py
+++ ueh-web-dev/backend/categories/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
+
+# app_name='categories'
+router = DefaultRouter()
+router.register(r'', views.ProductViewSet, basename='product')
 urlpatterns = [
-    path('products/', views.ProductList, name='product-list'),
-    # Thêm các URL khác nếu cần thiết
+    path('', include(router.urls)),
 ]
+# urlpatterns = [
+# 	path('', views.ProductViewSet.as_view(), name='products'),
+
+# ]
--- ueh-web-dev/backend/categories/views.py
+++ ueh-web-dev/backend/categories/views.py
@@ -1,16 +1,10 @@
-from rest_framework.decorators import api_view
+from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.response import Response
-from rest_framework import status
 
+# Create your views here.
 
-
-
-@api_view(['GET'])
-def ProductList(request):
-    if request.method =='GET':
-        products = Product.objects.all()  # Truy vấn tất cả các sản phẩm
-        serializer = ProductSerializer(products, many=True)  # Serialize dữ liệu
-        return Response(serializer.data, status=status.HTTP_200_OK)  # Trả về dữ liệu đã serialized
-    
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
