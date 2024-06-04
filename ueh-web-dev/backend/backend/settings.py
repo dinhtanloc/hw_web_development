84d6c3d3dbed676bc274475a6e06bad412356c52
LocUEH <locdinh.31221020226@st.ueh.edu.vn>
2024-06-04 22:05:37 +0700

    update admin  frontend interface

--- ueh-web-dev/backend/backend/settings.py
+++ ueh-web-dev/backend/backend/settings.py
@@ -121,7 +121,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
